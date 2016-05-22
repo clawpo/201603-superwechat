4f16dd9ad38e5fbbf65696b5b2fdd2b5f0ba0631
耿小泰 <clawpo@foxmail.com>
2016-05-22 18:24:50 +0800

    添加下载所有好友列表、下载所有群组和公开群组的人物类

---
+++ src/cn/ucai/superwechat/task/DownloadAllGroupTask.java
@@ -0,0 +1,60 @@
+package cn.ucai.superwechat.task;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.android.volley.Response;
+
+import java.util.ArrayList;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.activity.BaseActivity;
+import cn.ucai.superwechat.bean.Group;
+import cn.ucai.superwechat.data.ApiParams;
+import cn.ucai.superwechat.data.GsonRequest;
+import cn.ucai.superwechat.utils.Utils;
+
+public class DownloadAllGroupTask extends BaseActivity {
+    private static final String TAG = DownloadAllGroupTask.class.getName();
+    Context mContext;
+    String username;
+    String path;
+
+    public DownloadAllGroupTask(Context mContext, String username) {
+        this.mContext = mContext;
+        this.username = username;
+        initPath();
+    }
+
+    private void initPath() {
+        try {
+            path = new ApiParams()
+                    .with(I.User.USER_NAME,username)
+                    .getRequestUrl(I.REQUEST_DOWNLOAD_GROUPS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void execute(){
+        executeRequest(new GsonRequest<Group[]>(path,Group[].class,
+                responseDownloadAllGroupTaskListener(),errorListener()));
+    }
+
+    private Response.Listener<Group[]> responseDownloadAllGroupTaskListener() {
+        return new Response.Listener<Group[]>() {
+            @Override
+            public void onResponse(Group[] groups) {
+                if(groups!=null){
+                    ArrayList<Group> list = Utils.array2List(groups);
+                    ArrayList<Group> groupList =
+                            SuperWeChatApplication.getInstance().getGroupList();
+                    groupList.clear();
+                    groupList.addAll(list);
+                    mContext.sendStickyBroadcast(new Intent("update_group_list"));
+                }
+            }
+        };
+    }
+}
