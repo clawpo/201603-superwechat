f1b2d0cb64e25aafcdc8a8ba4568986ca076d23c
耿小泰 <clawpo@foxmail.com>
2016-06-16 23:23:04 +0800

    添加精品首页的布局

---
+++ src/cn/ucai/fulicenter/fragment/BoutiqueFragment.java
@@ -0,0 +1,22 @@
+package cn.ucai.fulicenter.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by clawpo on 16/6/16.
+ */
+public class BoutiqueFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_boutique,container,false);
+        return layout;
+    }
+}
